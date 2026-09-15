25104588b2a9529839585f554ff6ac2bb87adf85
ch6vip <ch6vip@users.noreply.github.com>
2026-09-15 21:27:46 +0800

    chore: bump version to 0.6.1 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "io.github.ch6vip.bilisb"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.6.1"
     }
 
     signingConfigs {
